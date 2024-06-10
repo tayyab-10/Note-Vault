c5ff986d57f557cf94ebafeb486851a28d20a15c
Muhammad Tayyab <tu.haider138@gmail.com>
2024-06-10 18:49:39 +0500

    Created user and stored in the database

--- Backend/Routes/auth.js
+++ Backend/Routes/auth.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const User = require("../Models/User")
 
-router.get('/', (req, res) => {
-    obj = {
-        Tayyab: "coder",
-        number: 133
-    }
-    res.json(obj);
+
+//Creating a user using the POST req "/api/auth"
+router.post('/', (req, res) => {
+    console.log(req.body);
+    const user = User(req.body);
+    user.save();
+    res.json(req.body);
 
 })
 module.exports = router

c771304bf4ae0bdca809709490d38002b4ca9034
Muhammad Tayyab <tu.haider138@gmail.com>
2024-06-11 22:42:16 +0500

    Updating an Existing Note in the Notevault

--- Backend/Routes/notes.js
+++ Backend/Routes/notes.js
@@ -48,5 +48,52 @@ router.get('/getAllNotes', fetchuser, async (req, res) => {
     }
 });
 
+
+// Route 3: Update an existing note using PUT req "/api/notes/updateNote/:id"
+router.put('/updateNote/:id',
+    fetchuser,
+    [
+        body('title', 'Title is required').isLength({ min: 1 }),
+        body('description', 'Description is required').isLength({ min: 1 }),
+        body('tag').optional().isLength({ min: 1 }),
+    ],
+    async (req, res) => {
+        // Handle validation errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+
+        const { title, description, tag } = req.body;
+
+        const newNote = {};
+        if (title) newNote.title = title;
+        if (description) newNote.description = description;
+        if (tag) newNote.tag = tag;
+
+        try {
+            let note = await Notes.findById(req.params.id);
+            if (!note) {
+                return res.status(404).send("Note not found");
+            }
+
+            if (note.user.toString() !== req.user.id) {
+                return res.status(401).send("Not Authorized");
+            }
+            // Update the note
+            note = await Notes.findByIdAndUpdate(
+                req.params.id,
+                { $set: newNote },
+                { new: true }
+            );
+
+            res.json(note);
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send({ error: "Internal Server Error" });
+        }
+
+    })
 module.exports = router;
 
